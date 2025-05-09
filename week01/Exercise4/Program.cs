03d03566a07e49f8e1ba949651231d823b2d4777
jkkitchen <jkitchen1@byupathway.edu>
2025-05-09 14:37:18 -0400

    Completed Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+
+        int sumNumbers = 0;
+        float avgNumbers = 0;
+        int maxNumber = -10000;
+        int smallestPositiveNumber = 100000;
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            sumNumbers = sumNumbers + numbers[i];
+            avgNumbers = ((float)sumNumbers) / numbers.Count;
+            if (maxNumber < numbers[i])
+            {
+                maxNumber = numbers[i];
+            }
+            if (numbers[i] > 0 && smallestPositiveNumber > numbers[i])
+            {
+                smallestPositiveNumber = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sumNumbers}");
+        Console.WriteLine($"The average is: {avgNumbers}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+
+        numbers.Sort();
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
