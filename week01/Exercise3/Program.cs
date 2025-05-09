dfe56424013a9d33cc7c67b85f3318a00420548f
jkkitchen <jkitchen1@byupathway.edu>
2025-05-09 13:53:16 -0400

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            int guesses = 1;
+
+            while (userGuess != magicNumber)
+            {
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guesses = guesses + 1;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guesses} tries.");
+
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+        } while (response == "Yes");
     }
 }
